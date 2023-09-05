3dc558e10cb84986ad5af571e9ef3d36e7803419
jk <projectjk@naver.com>
2023-09-05 12:37:42 +0900

    최초 커밋

--- src/main/java/com/jk/saraApi/main/MainService.java
+++ src/main/java/com/jk/saraApi/main/MainService.java
@@ -85,4 +85,8 @@ public class MainService extends CommonService {
 		//return ((BigInteger)paramMap.get("bucketSeqNo")).longValue();
 	}
 
+	public static void main(String[] args) throws Exception{
+		System.out.println(Encryptor.sha512( "Camac250#"));
+	}
+
 }
