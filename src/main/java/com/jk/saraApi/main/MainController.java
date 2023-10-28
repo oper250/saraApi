c561bf2ff164e2fdd89d638c84261e0cedcd4a9b
jk <projectjk@naver.com>
2023-10-28 09:00:00 +0900

    최초 커밋

--- src/main/java/com/jk/saraApi/main/MainController.java
+++ src/main/java/com/jk/saraApi/main/MainController.java
@@ -126,7 +126,7 @@ public class MainController extends CommonController {
     @ResponseBody
     @PostMapping( value = "/getUserInfo" )
     // 추천 버킷리스트 등록 (관리자용)
-    public Map<String, Object> getUserInfo(HttpServletRequest request, @RequestParam Map<String, Object> paramMap) throws Exception {
+    public Map<String, Object> getUserInfo(HttpServletRequest request, @RequestBody Map<String, Object> paramMap) throws Exception {
         Map<String, Object> rsMap = mainService.getUserInfo(paramMap);
 
         return super.getResponse(rsMap);
