7a28e4db6b031e2badcd1b9d07fa39e5ad749beb
jk <projectjk@naver.com>
2024-01-09 13:39:02 +0900

    최초 커밋

--- src/main/java/com/jk/saraApi/main/MainService.java
+++ src/main/java/com/jk/saraApi/main/MainService.java
@@ -186,7 +186,9 @@ public class MainService extends CommonService {
 		System.out.println("rsList.size() : " + rsList.size());
 
 		for (int i =0; i < rsList.size(); i++) {
-			suggestBucketGroup = (String) (rsList.get(i).get("suggestBucketGroup") );
+			Map<String, Object> suggestMap = rsList.get(i);
+			System.out.println("suggestMap ::" + suggestMap);
+			suggestBucketGroup = (String) suggestMap.get("suggestBucketGroup");
 			if("00".equals(suggestBucketGroup)) {
 				suggestList00.add(rsList.get(i));
 			} else if("01".equals(suggestBucketGroup)) {
